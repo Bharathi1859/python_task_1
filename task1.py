4dbcf38d0ca001a158926ab0cf93f29200d6e47c
Bharathi1859 <146173638+Bharathi1859@users.noreply.github.com>
2024-10-12 09:34:41 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,33 +1,56 @@
-l=list(input("enter list: "))
-print(l)
-l.append(3)
-print(l)
-l.pop()
-print(l)
+#list
+userlist=input("Enter the list elements: ")
+mylist=userlist.split()
+print("list",mylist)
+mylist.append("object")
+print("list",mylist)
+mylist.pop(2)
+print("list",mylist)
+mylist[1]="yellow"
+print("list",mylist)
+mylist.insert(3,"sky blue")
+print("list",mylist)
 
-t=tuple(input("enter tuple: "))
-x=list(t)
-x.append("t")
-print(tuple(x))
-x.pop()
-print(tuple(x))
 
-s=set(input("enter set: "))
-s.add("rat")
-print(s)
-s.remove("rat")
-print(s)
+#tuple
+usertuple=input("Enter the tuple elements: ")
+my_tuple=tuple(usertuple.split())
+print("tuple ",my_tuple)
+x=list(my_tuple)
+x.append("45")
+print("tuple ",tuple(x))
+x.insert(2,46)
+print("tuple ",tuple(x))
+x.pop(0)
+print("tuple ",tuple(x))
+x.reverse()
+print("tuple ",tuple(x))
 
-dict1={}
-dict=int(input("enter dict: "))
-for i in range(dict):
-    key=input("enter key")
-    value=input("enter value")
-    dict1[key]=value
-dict1[2]=8
-print(dict1)
+#set
+userset=input("Enter the set elements: ")
+my_set=set(userset.split(','))
+print("set",my_set)
+y={20,40,56}
+my_set.update(y)
+print("set ",my_set)
+my_set.clear()
+print("set ",my_set)
 
-print(l)
-print(t)
-print(s)
-print(dict1)
+#dictionary
+userdict=int(input("Enter the dictiobary: "))
+my_dict={}
+for i in range(userdict):
+    key=input("key: ")
+    my_dict[key]=input("values: ")
+print("dict",my_dict)
+my_dict["black"]=2
+print("dict",my_dict)
+my_dict.pop("red")
+print("dict",my_dict)
+my_dict.update({"pink":8})
+print("dict",my_dict)
+
+
+
+
+
